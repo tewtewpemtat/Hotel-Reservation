9a94f3261977a3b4edeb83341e67bbc15fb09577
tewtewpemtat <tew00202@gmail.com>
2023-03-09 23:39:18 +0700

    ja

--- index.php
+++ index.php
@@ -11,38 +11,51 @@ include('php/index.php');
   <body>
     <header>                        
       <h1>Hotel Reservation</h1>
-      <a href="logout.php" class="admin-link">Logout</a>
-    </header>     
-	<div class="all">                   
-    <div class="sidebar" > 
-      <!-- content1 -->
-      <form action="#" method="GET">
-        <input type="search" placeholder="Search...." name="q">
-        <button type="submit" class="bsearch">Search</button>
-        <select id="" name="">
-         <option value="">Province</option>
-          <option value=""></option>
-          <option value=""></option> 
+        <a href="logout.php" class="admin-link">Logout</a>
+      </header>     
+    <div class="all">                   
+      <div class="sidebar" > 
+        <!-- content1 -->
+        <form action="#" method="GET">
+          <input type="search" placeholder="Search...." name="q">
+          <button type="submit" class="bsearch">Search</button>
+          <br>
+          <br>
+          <label for="province">Province:</label>
+          <select name="province" id="province">
+            <option value="none">None</option>
+            <?php
+              $sql = "SELECT DISTINCT province FROM hotel";
+              foreach ($conn->query($sql) as $row) {
+                echo "<option value='" . $row['province'] . "'>" . $row['province'] . "</option>";
+              }
+            ?>
           </select>
-        <select id="" name="">
-         <option value="">District</option>
-          <option value=""></option>
-          <option value=""></option>
-        </select>
-      </form>
-        
+          <label for="district">District:</label>
+          <select name="district" id="district">
+            <option value="none">None</option>
+            <?php
+              $sql = "SELECT DISTINCT district FROM hotel";
+              foreach ($conn->query($sql) as $row) {
+                echo "<option value='" . $row['district'] . "'>" . $row['district'] . "</option>";
+              }
+            ?>
+          </select>
+          <button type="submit" class="bsearch">Go</button>
+        </form>
       <?php
+      
       if ($result->rowCount() > 0) {
         // output data of each row
-        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        while($row = $result ->fetch(PDO::FETCH_ASSOC)) {
           // Display the data here
           echo "<section class='hotel-details'>";
           echo "<div class='card'>";
           echo "<img src='data:image/jpeg;base64," . base64_encode($row['image']) . "' alt='Hotel Image'>";
           echo "<div class='card-details'>";
           echo "<h3 class='hotel-name'>" . $row['name'] . "</h3>";
-          echo "<p class='hotel-description'>" . $row['address'] . ", " . $row['province'] . "</p>";
-		  echo "<p class='hotel-availability'>Number of Rooms: " . $row['numberofrooms'] . "</p>";
+          echo "<p class='hotel-description'>" . $row['address'] . ", " . $row['province'] . ", " . $row['district'] . "</p>";
+		      echo "<p class='hotel-availability'>Number of Rooms: " . $row['numberofrooms'] . "</p>";
           echo "<a href='room.php?hotel_name=" . urlencode($row['name']) . "' class='book-now'>View Rooms</a>";
           echo "</div></div></section>";
         }
--- php/index.php
+++ php/index.php
@@ -4,12 +4,25 @@ if (!isset($_SESSION['email'])) {
     header("Location: login.php");
     exit;
 }
+$conn = new PDO("mysql:host=localhost;dbname=hotelreservation;charset=utf8","root","");
+$sql = "SELECT id, name, address, province, district, contact, image, numberofrooms FROM hotel";
 if(isset($_GET['q'])) {
     $search_query = $_GET['q'];
-    $sql = "SELECT id, name, address, province, district, contact, image, numberofrooms FROM hotel WHERE name LIKE '%$search_query%'";
-  } else {
+    $sql .= " WHERE name LIKE '%$search_query%'";
+} else {
     $sql = "SELECT id, name, address, province, district, contact, image, numberofrooms FROM hotel";
-  }
-    $conn = new PDO("mysql:host=localhost;dbname=hotelreservation;charset=utf8","root","");
-    $result = $conn->query($sql);
+}
+
+if(isset($_GET['province']) && $_GET['province'] !== 'none' && isset($_GET['district']) && $_GET['district'] !== 'none') {
+    $province = $_GET['province'];
+    $district = $_GET['district'];
+    $sql .= " AND province = '$province' AND district = '$district'";
+} elseif(isset($_GET['province']) && $_GET['province'] !== 'none') {
+    $province = $_GET['province'];
+    $sql .= " AND province = '$province'";
+} elseif(isset($_GET['district']) && $_GET['district'] !== 'none') {
+    $district = $_GET['district'];
+    $sql .= " AND district = '$district'";
+}
+$result = $conn->query($sql);
 ?>
